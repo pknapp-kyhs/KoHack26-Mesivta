ea77d053534cdaa0aae967ccf82ed7c427f367d9
Mendel W <mendel13w@gmail.com>
2026-03-22 12:27:01 -0400

    Photos 1

---
+++ KoHack26/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  KoHack26
+//
+//  Created by Mendel W on 3/22/26.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
---
+++ KoHack26/PageScan.swift
@@ -0,0 +1,18 @@
+//
+//  PageScan.swift
+//  KoHack26
+//
+//  Created by Mendel W on 3/22/26.
+//
+
+import SwiftUI
+
+struct PageScan: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageScan()
+}
