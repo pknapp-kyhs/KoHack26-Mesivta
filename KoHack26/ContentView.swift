3e79b662eef8392c59fec1ca4b273dc3234f23e1
Mendel W <mendel13w@gmail.com>
2026-03-22 11:18:14 -0400

    Initial Commit

---
+++ KoHack26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KoHack26
+//
+//  Created by Mendel W on 3/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KoHack26/KoHack26App.swift
@@ -0,0 +1,17 @@
+//
+//  KoHack26App.swift
+//  KoHack26
+//
+//  Created by Mendel W on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct KoHack26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
